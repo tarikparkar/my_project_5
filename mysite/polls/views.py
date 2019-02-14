793f0e10484ab075ef577a6bfd94d4dd67ebc1c5
Tarik Parkar <tarik_parkar@hotmail.com>
2019-02-14 15:06:32 +1100

    push changes to git

---
+++ mysite/polls/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse(" hello world. your at the polls. index")
+
+# Create your views here.
